51618326b5eafca2b02cbce59d80b95d48713dc6
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-05-01 16:56:06 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,49 +24,10 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-
-            groupId = "com.github.FoxyTheBot"
-            artifactId = "ValorantWrapper"
-            version = project.version.toString()
-
-            pom {
-                name.set("Valorant Wrapper")
-                description.set("A simple wrapper for HenrikDev's unofficial VALORANT API")
-                url.set("https://github.com/FoxyTheBot/ValorantWrapper")
-
-                licenses {
-                    license {
-                        name.set("AGPL-3.0 License")
-                        url.set("https://opensource.org/licenses/AGPL-3.0")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("WinG4merBR")
-                        name.set("WinG4merBR")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/FoxyTheBot/ValorantWrapper.git")
-                    developerConnection.set("scm:git:ssh://github.com/FoxyTheBot/ValorantWrapper.git")
-                    url.set("https://github.com/FoxyTheBot/ValorantWrapper")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            url = uri("https://jitpack.io")
-            credentials {
-                username = System.getenv("JITPACK_USERNAME") ?: project.findProperty("jitpack.username").toString()
-                password = System.getenv("JITPACK_TOKEN") ?: project.findProperty("jitpack.token").toString()
-            }
         }
     }
 }
+
 kotlin {
     jvmToolchain(21)
 }
