03e30a073ecc7c3a168b0979f033ebc27b301297
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-05-01 16:28:05 -0300

    Remove `val `from ApiException

--- src/main/kotlin/net/cakeyfox/foxy/valorant/wrapper/utils/ApiException.kt
+++ src/main/kotlin/net/cakeyfox/foxy/valorant/wrapper/utils/ApiException.kt
@@ -2,6 +2,6 @@ package net.cakeyfox.foxy.valorant.wrapper.utils
 
 import net.cakeyfox.foxy.valorant.wrapper.models.ApiErrorResponse
 
-class ApiException(private val error: ApiErrorResponse) : Exception(
+class ApiException(error: ApiErrorResponse) : Exception(
     "API Error ${error.status}: ${error.errors.joinToString { it.message }}"
 )
